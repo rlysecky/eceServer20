05a4348c9cc5fc5b1afea6cc16c97bd6528c447f
Roman Lysecky <roman@lysecky.com>
2020-10-22 07:51:08 -0700

    Added Holz user registration.

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
 var mongoose = require("mongoose");
 
-mongoose.set('useCreateIndex', true);
-
 mongoose.connect("mongodb://localhost/db20", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 module.exports = mongoose;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+let router = express.Router();
+let bcrypt = require("bcryptjs");
+let User = require('../models/users');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/register', function(req, res) {
+  bcrypt.hash(req.body.password, 10, function(err, hash) {
+    if (err) {
+      res.status(400).json({success : false, message : err.errmsg});  
+    }
+    else {
+      let newUser = new User({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        passwordHash: hash
+      });
+
+      newUser.save(function(err, user) {
+        if (err) {
+          res.status(400).json({success: false,
+                                message: err.errmsg});
+        }
+        else {
+          res.status(201).json({success: true,
+                                message: user.fullName + " has been created."});
+        }
+      });
+    }
+  });    
 });
 
 module.exports = router;
